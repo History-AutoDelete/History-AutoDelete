02e218a7b5deb51e8706ca71ae68b87e296e4c14
Kenny Do <mrdokenny@gmail.com>
2017-01-01 19:07:13 -0800

    Added functionality for settings page

--- settings/settings.js
+++ settings/settings.js
@@ -1,31 +1,39 @@
-function makeTableHTML(myArray) {
-    var result = "<table border=1>";
-    for(var i=0; i<myArray.length; i++) {
-        result += "<tr>";
-        for(var j=0; j<myArray[i].length; j++){
-            result += "<td>"+myArray[i][j]+"</td>";
-        }
-        result += "</tr>";
+function clickRemoved(event) {
+    if(event.target.classList.contains("url")) {
+        //console.log(event.target.parentElement.textContent);
+        page.removeURL(event.target.parentElement.textContent);
     }
-    result += "</table>";
-
-    return result;
 }
 
-browser.storage.local.get("URLS", function (result) {
-    var array = result.URLS;
-    console.log(array[0]);
-    var arrayLength = array.length;
-    var theTable = document.createElement('table');
+function generateTableOfURLS() {
+    browser.storage.local.get("URLS", function (result) {
+        var array = result.URLS;
+        var arrayLength = array.length;
+        var theTable = document.createElement('table');
 
-    // Note, don't forget the var keyword!
-    for (var i = 0, tr, td; i < arrayLength; i++) {
-        tr = document.createElement('tr');
-        td = document.createElement('td');
-        td.appendChild(document.createTextNode(array[i]));
-        tr.appendChild(td);
-        theTable.appendChild(tr);
-    }
+        for (var i = 0, tr, td; i < arrayLength; i++) {
+            tr = document.createElement('tr');
+            td = document.createElement('td');
+            var removeButton = document.createElement("button");
+            removeButton.classList.add("url");
+            removeButton.addEventListener("click", clickRemoved);
+            td.appendChild(removeButton);
+            td.appendChild(document.createTextNode(array[i]));
+            tr.appendChild(td);
+            theTable.appendChild(tr);
+        }
 
-    document.getElementById('table').appendChild(theTable);
+        document.getElementById('table').appendChild(theTable);
+    });
+}
+
+generateTableOfURLS();
+var page = browser.extension.getBackgroundPage();
+
+document.getElementById("clear").addEventListener("click", function(){
+    page.clearURL();
+})
+
+browser.storage.onChanged.addListener(function() {
+    location.reload();
 });
